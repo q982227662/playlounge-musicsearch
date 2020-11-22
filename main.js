b2aae7a1820d2dccc02878024fb0f3ba6be6fbda
q982227662 <63342235+q982227662@users.noreply.github.com>
2020-11-22 03:55:48 -0600

    itunes webpage updated

--- main.js
+++ main.js
@@ -1,69 +1,59 @@
-const fetchJsonp = require("fetch-jsonp");
+$(document).ready(function(){
+  
 
-//import validtion and showAlert
-import { showAlert, isEmpty } from "./validation";
-// Get searchform element 
-const searchForm = document.querySelector("#search-form"); 
-//Submit search form
-searchForm.addEventListener("submit", fetchMusic)
+    $("#search-form").submit(function (event){
+        event.preventDefault()
+        var search = $("#search-text").val()
+        var video = ''
 
-//Fetch music data from Apple music APi
-function fetchMusic(e){
-    e.preventDefault();
-    const searchText = document.querySelector("#search-text").value;
+        videoSearch(search)
+    })
+    function videoSearch(search){
 
-    if(isEmpty(searchText)){
-        showAlert("Please search something.","warning");
-        return;
+        $.ajax({
+            url:  "https://itunes.apple.com/search?term="+search,
+            dataType: "jsonp",
+            success: function( response ) {
+                console.log( response );
+                //clear results
+            results.innerHTML = '';
+            // loop through every music in the element by checking the song
+            if(results.count ===0){
+                showAlert('Nothing Found!','danger');
+                return;
+        
+            }    
+            const outputs = document.querySelector("#results");
+
+            outputs.innerHTML = '';
+            response.results.forEach(results => {    
+                    //clear results
+                    // loop through every music in the element by checking the song
+                
+                        const div = document.createElement("div");
+                        div.classList.add("card");
+                        div.innerHTML = 
+                        `<img class="card-img-top" src=${results.artworkUrl100} alt="Album Artwork">
+                        <div class="card-body">
+                          <h5 class="card-title">${results.trackName}</h5>
+                          <p class="card-text">${results.artistName}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">${results.collectionName}</li>
+                          <li class="list-group-item">${results.primaryGenreName} . ${results.releaseDate.split('-',1)}</li>
+                
+                          <li class="list-group-item">sample: <br>
+                                <audio src =${results.previewUrl} controls='controls'>
+                                </audio></li>
+                        </ul>
+                        <div class="card-body">
+                          <a href=${results.trackViewUrl} class="card-link">Show in itunes</a>
+                
+                        </div>`;
+                        outputs.appendChild(div);
+                });
+            }
+        });
     }
-    const fetchUrl = `https://itunes.apple.com/search?term=${searchText}`;
-    
-    fetchJsonp(fetchUrl)
-    .then(res  => res.json())
-    .then(data => showMusic(data.results))
-    .catch(err => console.log(err));
-}
+})
 
-
-//show each music in a format of card
-function showMusic(musics){
-    console.log(musics);
-    const results = document.querySelector("#results");
-    if(musics.length ===0){
-        showAlert('Nothing Found!','danger');
-        return;
-
-    }
-    
-    //clear results
-    results.innerHTML = '';
-    // loop through every music in the element by checking the song
-    for(let i=0; i<musics.length; i++){
-        const music = musics[i];
-        if(music.kind !== "song"){
-            continue;
-        }
-
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.innerHTML = 
-        `<img class="card-img-top" src=${music.artworkUrl100} alt="Album Artwork">
-        <div class="card-body">
-          <h5 class="card-title">${music.trackName}</h5>
-          <p class="card-text">${music.artistName}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">${music.collectionName}</li>
-          <li class="list-group-item">${music.primaryGenreName} . ${music.releaseDate.split('-',1)}</li>
-
-          <li class="list-group-item">sample: <br>
-                <audio src =${music.previewUrl} controls='controls'>
-                </audio></li>
-        </ul>
-        <div class="card-body">
-          <a href=${music.trackViewUrl} class="card-link">Show in itunes</a>
-
-        </div>`;
-        results.appendChild(div);
-    }
-}
